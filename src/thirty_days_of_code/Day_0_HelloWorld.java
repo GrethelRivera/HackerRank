1aebfd5151ac0cf9554bbd1416c62740d7bd3934
GrethelRivera <grethelr91@gmail.com>
2022-10-15 14:00:49 -0400

    First Commit

---
+++ src/thirty_days_of_code/Day_0_HelloWorld.java
@@ -0,0 +1,18 @@
+package thirty_days_of_code;
+
+import java.util.Scanner;
+
+public class Day_0_HelloWorld {
+//    public class Solution {
+
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+            Scanner readCode = new Scanner(System.in);
+            String input = readCode.nextLine();
+            System.out.print("Hello, World.\n");
+            System.out.print(input);
+
+        }
+    }
+
