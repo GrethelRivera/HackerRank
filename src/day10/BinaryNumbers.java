4e649ebfae32f4d1cbcc52040c7bbc4f0857494e
GrethelRivera <grethelr91@gmail.com>
2022-10-24 21:34:39 -0400

    Commit - Day 10

---
+++ src/day10/BinaryNumbers.java
@@ -0,0 +1,19 @@
+package day10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BinaryNumbers {
+
+
+  //  public class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            bufferedReader.close();
+        }
+    }
+
